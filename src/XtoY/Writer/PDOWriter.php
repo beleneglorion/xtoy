9363b5f7c0b0ffb308822e0c10b06ae9893351eb
Sebastien THIBAULT <sebastien@oblady.fr>
2014-01-13 14:01:51 +0100

    manage duplicate entries add drivers options

--- src/XtoY/Writer/PDOWriter.php
+++ src/XtoY/Writer/PDOWriter.php
@@ -26,7 +26,9 @@ class PDOWriter  extends Optionnable implements WriterInterface
         $this->addRequiredOption('table');
         $this->addOption('username',null);
         $this->addOption('password',null);
+        $this->addOption('driver_options',null);
         $this->addOption('transaction',false);
+        $this->addOption('ignore_duplicate',false);
         $this->getOptionManager()->init($options);
 
    }
@@ -45,15 +47,19 @@ class PDOWriter  extends Optionnable implements WriterInterface
        if (!isset($this->dbh)) {
 
             $options = $this->getOptions();
+            $driverOptions = array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION);
             $dsn = $this->getDDN();
+            if(isset($options['driver_options'])) {
+                $driverOptions = $driverOptions+ $options['driver_options'];
+            }
             try {
-                $this->dbh = new \PDO($dsn, $options['username'], $options['password']);
+                $this->dbh = new \PDO($dsn, $options['username'], $options['password'], $driverOptions);
             } catch (\PDOException $e) {
                 throw new \Exception(sprintf('Can\'t connect to database %s (%s)',$dsn,$e->getMessage()));
             }
             if ($options['transaction']) {
                $this->dbh->beginTransaction();
-            }
+            }       
        }
 
    }
@@ -72,14 +78,24 @@ class PDOWriter  extends Optionnable implements WriterInterface
 
     public function write($line)
     {
-
+        
         $options = $this->getOptions();
-        $keys = '"'.implode('","',array_map('trim',array_keys($line))).'"';
+        $keys = ''.implode(',',array_map('trim',array_keys($line))).'';
         $marks = implode(',',array_fill(0, count(array_keys($line)), '?'));
         $sql = sprintf('INSERT INTO %s (%s) VALUES(%s)'."\n",$options['table'],$keys,$marks);
         $sth = $this->dbh->prepare($sql);
+        try {
         $sth->execute(array_values($line));
+        } 
+        catch (\PDOException $e)
+        {
+            if(!$options['ignore_duplicate'] || $e->getCode() != 23000) {
+                throw $e;
+            }
 
+        }
+        
+        
     }
 
     public function writeAll($table)
